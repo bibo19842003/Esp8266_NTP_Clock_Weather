d858592e5829c18c530ae80255458df67ae549ea
bibo <bibo19842003@163.com>
2021-01-30 00:12:42 +0800

    list the wifi name for scan.

--- newweather.ino
+++ newweather.ino
@@ -169,10 +169,57 @@ bool autoConfig() {
 
 
 ESP8266WebServer server(80);
-String str = "<!DOCTYPE html><html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\"><title>ESP8266网页配网</title><script type=\"text/javascript\">function wifi(){var ssid = aa.value;var password = bb.value;var xmlhttp=new XMLHttpRequest();xmlhttp.open(\"GET\",\"/HandleWifi?ssid=\"+ssid+\"&password=\"+password,true);xmlhttp.send();xmlhttp.onload = function(e){alert(this.responseText);}}</script></head><body>hello,我是fxy,输入wifi信息给wifi时钟配网:  <form>WiFi名称：<input type=\"text\" placeholder=\"请输入您WiFi的名称\" id=\"aa\"><br>WiFi密码：<input type=\"text\" placeholder=\"请输入您WiFi的密码\" id=\"bb\"><br><input type=\"button\" value=\"连接\" onclick=\"wifi()\"></form></body></html>";
+String HTML_TITLE = "<!DOCTYPE html><html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\"><title>ESP8266网页配网</title>";
+String HTML_SCRIPT_ONE = "<script type=\"text/javascript\">function wifi(){var ssid = aa.value;var password = bb.value;var xmlhttp=new XMLHttpRequest();xmlhttp.open(\"GET\",\"/HandleWifi?ssid=\"+ssid+\"&password=\"+password,true);xmlhttp.send();xmlhttp.onload = function(e){alert(this.responseText);}}</script>";
+String HTML_HEAD_BODY_BEGIN = "</head><body>请输入wifi信息进行配网:";
+String HTML_FORM_ONE = "<form>WiFi名称：<input type=\"text\" placeholder=\"请输入您WiFi的名称\" id=\"aa\"><br>WiFi密码：<input type=\"text\" placeholder=\"请输入您WiFi的密码\" id=\"bb\"><br><input type=\"button\" value=\"扫描\" onclick=\"window.location.href = '/HandleScanWifi'\"><input type=\"button\" value=\"连接\" onclick=\"wifi()\"></form>";
+String HTML_BODY_HTML_END = "</body></html>";
+
+// String str = "<!DOCTYPE html><html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\"><title>ESP8266网页配网</title><script type=\"text/javascript\">function wifi(){var ssid = aa.value;var password = bb.value;var xmlhttp=new XMLHttpRequest();xmlhttp.open(\"GET\",\"/HandleWifi?ssid=\"+ssid+\"&password=\"+password,true);xmlhttp.send();xmlhttp.onload = function(e){alert(this.responseText);}}</script></head><body>hello,我是fxy,输入wifi信息给wifi时钟配网:  <form>WiFi名称：<input type=\"text\" placeholder=\"请输入您WiFi的名称\" id=\"aa\"><br>WiFi密码：<input type=\"text\" placeholder=\"请输入您WiFi的密码\" id=\"bb\"><br><input type=\"button\" value=\"连接\" onclick=\"wifi()\"></form></body></html>";
 
 void handleRoot() {
+    Serial.println("root page");
+    String str = HTML_TITLE + HTML_SCRIPT_ONE + HTML_HEAD_BODY_BEGIN + HTML_FORM_ONE + HTML_BODY_HTML_END;
     server.send(200, "text/html", str);
+}
+
+void HandleScanWifi() {
+    Serial.println("scan start");
+
+    String HTML_FORM_TABLE_BEGIN = "<table><head><tr><th>序号</th><th>名称</th><th>强度</th></tr></head><body>";
+    String HTML_FORM_TABLE_END = "</body></table>";
+    String HTML_FORM_TABLE_CON = "";
+    String HTML_TABLE;
+    // WiFi.scanNetworks will return the number of networks found
+    int n = WiFi.scanNetworks();
+    Serial.println("scan done");
+    if (n == 0) {
+        Serial.println("no networks found");
+        HTML_TABLE = "NO WIFI !!!";
+    }
+    else {
+        Serial.print(n);
+        Serial.println(" networks found");
+        for (int i = 0; i < n; ++i) {
+      // Print SSID and RSSI for each network found
+            Serial.print(i + 1);
+            Serial.print(": ");
+            Serial.print(WiFi.SSID(i));
+            Serial.print(" (");
+            Serial.print(WiFi.RSSI(i));
+            Serial.print(")");
+            Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
+            delay(10);
+            HTML_FORM_TABLE_CON = HTML_FORM_TABLE_CON + "<tr><td align=\"center\">" + String(i+1) + "</td><td align=\"center\">"+ WiFi.SSID(i) + "</td><td align=\"center\">" + WiFi.RSSI(i) + "</td></tr>";
+        }
+
+        HTML_TABLE = HTML_FORM_TABLE_BEGIN + HTML_FORM_TABLE_CON + HTML_FORM_TABLE_END;
+    }
+    Serial.println("");
+
+    String scanstr = HTML_TITLE + HTML_SCRIPT_ONE + HTML_HEAD_BODY_BEGIN + HTML_FORM_ONE + HTML_TABLE + HTML_BODY_HTML_END;
+    
+    server.send(200, "text/html", scanstr);
 }
 
 void HandleWifi() {
@@ -210,10 +257,12 @@ void htmlConfig() {
     
     server.on("/", handleRoot);
     server.on("/HandleWifi", HTTP_GET, HandleWifi);
+    server.on("/HandleScanWifi", HandleScanWifi);
     server.onNotFound(handleNotFound);//请求失败回调函数
     MDNS.addService("http", "tcp", 80);
     server.begin();//开启服务器
     Serial.println("HTTP server started");
+
     int counter = 0;
     while(1)
     {
